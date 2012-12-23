57c582a3df9570fe8fee25305c01de7a1ca14d86
Anders Olme <anders.olme@gmail.com>
2012-12-23 15:01:03 +0100

    first checkin

---
+++ cell.cc
@@ -0,0 +1,18 @@
+/*
+ * cell.cpp
+ *
+ *  Created on: Dec 23, 2012
+ *      Author: anders
+ */
+
+#include "/media/home/users/anders/ovn/c++/gof/cell.h"
+
+cell::cell() {
+	// TODO Auto-generated constructor stub
+
+}
+
+cell::~cell() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ cell.h
@@ -0,0 +1,17 @@
+/*
+ * cell.h
+ *
+ *  Created on: Dec 23, 2012
+ *      Author: anders
+ */
+
+#ifndef CELL_H_
+#define CELL_H_
+
+class cell {
+public:
+	cell();
+	virtual ~cell();
+};
+
+#endif /* CELL_H_ */
